f85550950dd599dff5e9160b7e2dc3bbe38c500b
Nikolaj Boel Jensen <nbj@lindekrogen.dk>
2024-02-23 11:17:46 +0100

    Add missing doc blocks

--- events.go
+++ events.go
@@ -2,14 +2,20 @@ package Nbj
 
 import "reflect"
 
+// Event
+// Interface that describes an event
 type Event interface {
 	Accessor() string
 }
 
+// Listener
+// Interface that describes a listener
 type Listener interface {
 	Handle(event any) bool
 }
 
+// EventDispatchReport
+// Structure describing an event dispatch report
 type EventDispatchReport struct {
 	Registered      bool
 	Completed       bool
@@ -17,16 +23,22 @@ type EventDispatchReport struct {
 	ListenerReports []ListenerDispatchReport
 }
 
+// ListenerDispatchReport
+// Structure describing a listener dispatch report
 type ListenerDispatchReport struct {
 	Instance           string
 	Completed          bool
 	StoppedPropagation bool
 }
 
+// EventHandler
+// Structure describing the actual event handler
 type EventHandler struct {
 	events map[string][]Listener
 }
 
+// NewEventHandler
+// Named constructor that initializes a new event handler
 func NewEventHandler() *EventHandler {
 	var instance EventHandler
 	instance.events = make(map[string][]Listener)
@@ -34,12 +46,16 @@ func NewEventHandler() *EventHandler {
 	return &instance
 }
 
+// SetEventsAndListeners
+// Overrides all events and associated listener with a complete configuration
 func (handler *EventHandler) SetEventsAndListeners(events map[string][]Listener) *EventHandler {
 	handler.events = events
 
 	return handler
 }
 
+// Register
+// Registers an event on the event handler
 func (handler *EventHandler) Register(accessor string) *EventHandler {
 	// We do not want to override existing events and listener
 	// So if the event is already registered we simply return
@@ -53,6 +69,8 @@ func (handler *EventHandler) Register(accessor string) *EventHandler {
 	return handler
 }
 
+// AddListener
+// Adds a listener to an event. The event is automatically registered if it was not beforehand
 func (handler *EventHandler) AddListener(accessor string, listener Listener) *EventHandler {
 	// If the event accessor we are adding a listener to does not exist
 	// we make sure to register the event accessor
@@ -66,6 +84,9 @@ func (handler *EventHandler) AddListener(accessor string, listener Listener) *Ev
 	return handler
 }
 
+// Dispatch
+// Dispatches an event triggering all its listeners. This will generate and
+// return an event dispatch report
 func (handler *EventHandler) Dispatch(event Event) *EventDispatchReport {
 	eventReport := EventDispatchReport{
 		Registered: false,
